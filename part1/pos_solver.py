fcb92b85063c5c2342c634040d226c2da63181ee
Pulkit Maloo <maloop@iu.edu>
2017-11-11 17:11:57 -0500

    small prob instead of 0 -> Improved accuracy

--- part1/pos_solver.py
+++ part1/pos_solver.py
@@ -89,10 +89,13 @@ import numpy as np
 #
 class Solver:
 
+    SMALL_PROB = 1/float(10**6)
+    initial_all = {}
     initial = {}
     transition = {}
     emission = {}
     words_in_training = []
+
     # Calculate the log of the posterior probability of a given sentence
     #  with a given part-of-speech labeling
     def posterior(self, sentence, label):
@@ -105,25 +108,24 @@ class Solver:
         # Initial Probability
         ##############################################################
         for line in data:
-            ##### first word
+            ##### considering only first word
             self.initial[line[1][0]] = self.initial.get(line[1][0], 0) + 1
-            #### all words
-            #for S in line[1]:
-            #     self.initial[S] = self.initial.get(S, 0) + 1
+            ##### considering all words
+            for S in line[1]:
+                 self.initial_all[S] = self.initial_all.get(S, 0) + 1
 
         ##############################################################
         # Transition Probability
         ##############################################################
         states = self.initial.keys()
-        # Initialize transition probability with zeros.
-        for S in states:
-            self.transition[S] = {}
-            for S_prime in states:
-                self.transition[S][S_prime] = 0
 
         for line in data:
             for S, S_prime in zip(line[1], line[1][1:]):
-                self.transition[S][S_prime] += 1
+                try:
+                    self.transition[S][S_prime] = self.transition[S].get(S_prime, 0) + 1
+                except:
+                    self.transition[S] = {}
+                    self.transition[S][S_prime] = self.transition[S].get(S_prime, 0) + 1
 
         ##############################################################
         # Emission Probability
@@ -135,13 +137,17 @@ class Solver:
             for W, S in zip(line[0], line[1]):
                 self.emission[S][W] = self.emission[S].get(W, 0) + 1
                 # Initialize emission probabilities with 0 for all possible W
-                for S_other in states:
-                    if W not in self.emission[S_other]:
-                        self.emission[S_other][W] = 0
+#                for S_other in states:
+#                    if W not in self.emission[S_other]:
+#                        self.emission[S_other][W] = 0
+                self.words_in_training.append(W)
 
-        self.words_in_training = self.emission[S].keys()
+#        self.words_in_training = self.emission[S].keys()
 
         # Convert Counts to probabilities
+        S_total = sum(self.initial_all.values())
+        for S in self.initial_all:
+            self.initial_all[S] /= S_total
         S_total = sum(self.initial.values())
         for S in self.initial:
             self.initial[S] /= S_total
@@ -158,18 +164,19 @@ class Solver:
     #
     def simplified(self, sentence):
         ##### P(S | W) = P(W | S) * P(S) / P(W)
-        states = self.initial.keys()
-        sentence_states = []
+        states = self.initial_all.keys()
+        predicted_states = []
         for word in sentence:
-            probs_states = sorted([(self.emission[state].get(word, 1) * self.initial[state], state) for state in states], key = lambda x: x[0])
-            sentence_states.append(probs_states[-1][1])
+            most_prob_state = max([ (st, self.emission[st].get(word, self.SMALL_PROB) * self.initial_all[st]) \
+                                        for st in states ], key = lambda x: x[1])
+            predicted_states.append(most_prob_state[0])
 #            max_prob, most_prob_state = 0, ''
 #            for state in states:
 #                P_state_given_word = self.emission[state].get(word, 1) * self.initial[state]
 #                if P_state_given_word > max_prob:
 #                    max_prob, most_prob_state = P_state_given_word, state
 #            sentence_states.append(most_prob_state)
-        return sentence_states
+        return predicted_states
 
     def hmm_ve(self, sentence):
         states = self.initial.keys()
@@ -184,13 +191,11 @@ class Solver:
 
             for j, st in enumerate(states):
                 if i == 0:
-                    score[j][i] = self.initial[st] * self.emission[st].get(obs, 1)
+                    score[j][i] = self.initial[st] * self.emission[st].get(obs, self.SMALL_PROB)
                 else:
-                    p = 0
-                    for k, key in enumerate(self.transition):
-                        #if score[k][i-1] * self.transition[key][st] * self.emission[st][obs] > maximum:
-                        p += score[k][i-1] * self.transition[key][st] * self.emission[st].get(obs, 1)
-                    score[j][i] = p
+                    p = sum( [score[k][i-1] * self.transition[key].get(st, self.SMALL_PROB) \
+                                for k, key in enumerate(self.transition)] )
+                    score[j][i] = p * self.emission[st].get(obs, self.SMALL_PROB)
 
                 if score[j][i] > max_value:
                     max_value, max_state = score[j][i], st
@@ -208,21 +213,16 @@ class Solver:
         for i, obs in enumerate(observed):
             for j, st in enumerate(states):
                 if i == 0:
-                    score[j][i] = self.initial[st] * self.emission[st].get(obs, 1)
+                    score[j][i] = self.initial[st] * self.emission[st].get(obs, self.SMALL_PROB)
                     trace[j][i] = 0
                     #print score[j][i]
                 else:
-                    maximum, max_k  = 0, j
-                    for k, key in enumerate(self.transition):
-                        #### take logs ###
-                        p = score[k][i-1] * self.transition[key][st] * self.emission[st].get(obs, 1)
-                        if p > maximum:
-                            maximum, max_k = p, k
-                    score[j][i], trace[j][i] = maximum, max_k
+                    max_k, maximum = max([ (k, score[k][i-1] * self.transition[key].get(st, self.SMALL_PROB)) \
+                                           for k, key in enumerate(self.transition)], key = lambda x: x[1])
+                    score[j][i], trace[j][i] = maximum * self.emission[st].get(obs, self.SMALL_PROB), max_k
         # trace back
         z = np.argmax(score[:,-1])
         hidden = [states[z]]
-
         for i in range(len(observed)-1, 0, -1):
             z = trace[z,i]
             hidden.append(states[z])
@@ -244,4 +244,6 @@ class Solver:
             return self.hmm_viterbi(sentence)
         else:
             print "Unknown algo!"
+
+
 

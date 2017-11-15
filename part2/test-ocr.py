a2dc238e8dc0f7f7e58cb2ad41b4606414794943
Pulkit Maloo <maloop@iu.edu>
2017-11-14 19:59:40 -0500

    Run this script to test

--- part2/test-ocr.py
+++ part2/test-ocr.py
@@ -8,6 +8,6 @@ fname = "test-strings.txt"
 with open(fname) as fhand:
     for i in range(19):
         print "####################################"
-        print fhand.readline(),
+        print "Correct:", fhand.readline(),
         eval("""runfile('C:/Users/PulkitMaloo/Box Sync/Courses/AI/Assignment_3/hankjang-maloop-shynaras-a3/part2/ocr.py', args='courier-train.png bc.train test-"""
                         + str(i) + """-0.png', wdir='C:/Users/PulkitMaloo/Box Sync/Courses/AI/Assignment_3/hankjang-maloop-shynaras-a3/part2')""")
